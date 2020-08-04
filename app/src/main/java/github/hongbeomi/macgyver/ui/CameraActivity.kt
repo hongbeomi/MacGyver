1ba4544686d317c8bd6a1355a47025ef163d22bb
hongbeomi <dks3963@naver.com>
2020-08-04 14:38:07 +0900

    set camera layout

---
+++ app/src/main/java/github/hongbeomi/macgyver/ui/CameraActivity.kt
@@ -0,0 +1,14 @@
+package github.hongbeomi.macgyver.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import github.hongbeomi.macgyver.R
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
