87a8869cc6484e2df22e8c94ab2c04ce64623c16
hongbeomi <dks3963@naver.com>
2020-08-06 18:43:31 +0900

    remove unusing class & barcode test complete

--- app/src/main/java/github/hongbeomi/macgyver/mlkit/barcode_scan/BarcodeScannerProcessor.kt
+++ app/src/main/java/github/hongbeomi/macgyver/mlkit/barcode_scan/BarcodeScannerProcessor.kt
@@ -1,22 +1,55 @@
-//package github.hongbeomi.macgyver.mlkit.barcode_scan
-//
-//import com.google.android.gms.tasks.Task
-//import com.google.mlkit.vision.barcode.Barcode
-//import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-//import com.google.mlkit.vision.barcode.BarcodeScanning
-//import github.hongbeomi.macgyver.camerax.BaseImageAnalyzer
-//
-//class BarcodeScannerProcessor(override var doOnSuccess: (List<Barcode>) -> Unit) : BaseImageAnalyzer<List<Barcode>>() {
-//
-//    private val options = BarcodeScannerOptions.Builder()
-//        .setBarcodeFormats(
-//            Barcode.FORMAT_QR_CODE,
-//            Barcode.FORMAT_AZTEC
-//        )
-//        .build()
-//    private val scanner = BarcodeScanning.getClient(options)
-//
-//    override val process: Task<List<Barcode>>?
-//        get() = image?.let { scanner.process(it) }
-//
-//}
+package github.hongbeomi.macgyver.mlkit.barcode_scan
+
+import android.graphics.Rect
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import github.hongbeomi.macgyver.camerax.BaseImageAnalyzer
+import github.hongbeomi.macgyver.camerax.GraphicOverlay
+import java.io.IOException
+
+class BarcodeScannerProcessor(private val view: GraphicOverlay) : BaseImageAnalyzer<List<Barcode>>() {
+
+    private val options = BarcodeScannerOptions.Builder().build()
+    private val scanner = BarcodeScanning.getClient(options)
+
+    override val graphicOverlay: GraphicOverlay
+        get() = view
+
+    override fun detectInImage(image: InputImage): Task<List<Barcode>> {
+        return scanner.process(image)
+    }
+
+    override fun stop() {
+        try {
+            scanner.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Exception thrown while trying to close Barcode Scanner: $e")
+        }
+    }
+
+    override fun onSuccess(
+        results: List<Barcode>,
+        graphicOverlay: GraphicOverlay,
+        rect: Rect
+    ) {
+        graphicOverlay.clear()
+        results.forEach {
+            val barcodeGraphic = BarcodeGraphic(graphicOverlay, it, rect)
+            graphicOverlay.add(barcodeGraphic)
+        }
+        graphicOverlay.postInvalidate()
+    }
+
+    override fun onFailure(e: Exception) {
+        Log.w(TAG, "Barcode Scan failed.$e")
+    }
+
+    companion object {
+        private const val TAG = "BarcodeScanProcessor"
+    }
+
+}
--- app/src/main/java/github/hongbeomi/macgyver/ui/CameraActivity.kt
+++ app/src/main/java/github/hongbeomi/macgyver/ui/CameraActivity.kt
@@ -13,7 +13,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import github.hongbeomi.macgyver.R
 import github.hongbeomi.macgyver.databinding.ActivityCameraBinding
-import github.hongbeomi.macgyver.mlkit.object_detection.ObjectDetectionProcessor
+import github.hongbeomi.macgyver.mlkit.barcode_scan.BarcodeScannerProcessor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -65,7 +65,7 @@ class CameraActivity : BaseActivity() {
                     .also {
                         it.setAnalyzer(
                             cameraExecutor,
-                            ObjectDetectionProcessor(binding.graphicOverlayFinder)
+                            BarcodeScannerProcessor(binding.graphicOverlayFinder)
                         )
                     }
 
